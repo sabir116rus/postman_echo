ad0ad583db66f26e6a1331c1ed5f1c9e82250072
Альберт <sabir116rus@gmail.com>
2022-11-15 07:29:05 +0300

    repository initialization

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchoTest {
+    @Test
+    public void ShouldReturnSomeData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(/* --> ваша проверка здесь <-- */)
+        ;
+    }
+}
