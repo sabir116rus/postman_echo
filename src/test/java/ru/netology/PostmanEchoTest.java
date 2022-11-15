3c4ddf6923ad1db98d3318f4635bced54f26d65c
Альберт <sabir116rus@gmail.com>
2022-11-15 07:53:25 +0300

    fix test

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo ("Netologu"))
+                .body("data", equalTo ("Netology"))
         ;
     }
 }
